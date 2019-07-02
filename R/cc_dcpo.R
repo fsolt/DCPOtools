6f20d00561f78930ecdbd453d3b382c16b242f58
Frederick Solt <frederick-solt@uiowa.edu>
2019-07-01 19:44:38 -0500

    add cc_dcpo

---
+++ R/cc_dcpo.R
@@ -0,0 +1,20 @@
+#' Custom Country Names Used by DCPO
+#'
+#' A dataframe of custom country names for use with countrycode::countrycode
+#'
+#' \itemize{
+#'   \item country.name.en
+#'   \item country.name.en.regex
+#'   \item iso2c
+#'   \item iso3c
+#'   \item wb_api3c
+#'   \item dcpo.name
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name cc_dcpo
+#'
+#' @usage cc_dcpo
+#' @format A dataframe with six columns
+NULL
