8b73a79cf45d3179f7d35744014de003e1aa1502
Frederick Solt <frederick-solt@uiowa.edu>
2019-12-11 19:24:52 -0600

    add neb_combo

--- data-raw/surveys_data.R
+++ data-raw/surveys_data.R
@@ -6,10 +6,3 @@ surveys_data <- read_csv("data/surveys_data.csv",
                          col_type = cols(year = "i", .default = "c"))
 save(surveys_data, file = "data/surveys_data.rda")
 usethis::use_data(surveys_data, overwrite = TRUE)
-rm(surveys_data)
-
-devtools::install('.', local=FALSE)
-
-library(DCPOtools)
-library(tidyverse)
-beepr::beep()
