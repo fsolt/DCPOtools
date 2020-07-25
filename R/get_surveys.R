009ce8236b8a474468c1136d9eccb510c6b795e9
Frederick Solt <frederick-solt@uiowa.edu>
2020-07-25 09:21:49 -0500

    add wvs7

--- R/get_surveys.R
+++ R/get_surveys.R
@@ -471,6 +471,78 @@ get_surveys <- function(vars,
     rD[["server"]]$stop()
   }
 
+  wvs7_ds <- ds %>%
+    filter(survey == "wvs7")
+  if (nrow(wvs7_ds) > 0) {
+    new_dir <- wvs7_ds$new_dir[1]
+
+    # build path to chrome's default download directory
+    if (Sys.info()[["sysname"]]=="Linux") {
+      default_dir <- file.path("home", Sys.info()[["user"]], "Downloads")
+    } else {
+      default_dir <- file.path("", "Users", Sys.info()[["user"]], "Downloads")
+    }
+
+    # get list of current default download directory contents
+    dd_old <- list.files(default_dir)
+
+    # create target directory if necessary
+    dir.create(new_dir, recursive = TRUE, showWarnings = FALSE)
+    nd_old <- list.files(new_dir)
+
+    wvs_page <- "http://www.worldvaluessurvey.org/WVSDocumentationWV7.jsp"
+    rD <- RSelenium::rsDriver(browser = "chrome")
+    remDr <- rD[["client"]]
+    remDr$navigate(wvs_page)
+    webElem <- remDr$findElement(using = "tag name", "body")
+    webElem$clickElement()
+    webElem$sendKeysToElement(list(key = "end"))
+    Sys.sleep(7)
+    elem <- remDr$findElements(using = "tag name", "iframe")
+    remDr$switchToFrame(elem[[1]])
+    Sys.sleep(5)
+    elem1 <- remDr$findElements(using = "tag name", "iframe")
+    remDr$switchToFrame(elem1[[1]])
+    Sys.sleep(2)
+    remDr$findElement(using = "partial link text", "stata")$clickElement()
+    Sys.sleep(3)
+    remDr$findElement(using = "name", "LINOMBRE")$sendKeysToElement(list(getOption("pew_name")))
+    remDr$findElement(using = "name", "LIEMPRESA")$sendKeysToElement(list(getOption("pew_org")))
+    remDr$findElement(using = "name", "LIEMAIL")$sendKeysToElement(list(getOption("pew_email")))
+    acad_proj <- "a"
+    remDr$findElement(using = 'xpath', "//select")$sendKeysToElement(list(acad_proj))
+    webElem <- remDr$findElement(using = "tag name", "body")
+    webElem$clickElement()
+    webElem$sendKeysToElement(list(key = "end"))
+    remDr$findElement(using = "name", "LIAGREE")$clickElement()
+    remDr$findElement(using = "class", "AJDocumentDownloadBtn")$clickElement()
+    remDr$acceptAlert()
+
+    # check that download has completed
+    dd_new <- list.files(default_dir)[!list.files(default_dir) %in% dd_old]
+    wait <- TRUE
+    tryCatch(
+      while(all.equal(stringr::str_detect(dd_new, "\\.part$"), logical(0))) {
+        Sys.sleep(1)
+        dd_new <- list.files(default_dir)[!list.files(default_dir) %in% dd_old]
+      }, error = function(e) 1 )
+    while(any(stringr::str_detect(dd_new, "\\.crdownload$"))) {
+      Sys.sleep(1)
+      dd_new <- list.files(default_dir)[!list.files(default_dir) %in% dd_old]
+    }
+
+    # unzip into specified directory and convert to .RData
+    unzip(file.path(default_dir, dd_new), exdir = file.path(new_dir))
+    unlink(file.path(default_dir, dd_new))
+
+    data_file <- list.files(new_dir)[!list.files(new_dir) %in% nd_old]
+    haven::read_dta(file.path(new_dir, data_file), encoding = "latin1") %>%
+      rio::export(file.path(new_dir, paste0(wvs7_ds$survey[1], ".RData")))
+
+    remDr$close()
+    rD[["server"]]$stop()
+  }
+
   # LatinoBarometro
   lb_ds <- ds %>%
     filter(surv_program == "lb")
